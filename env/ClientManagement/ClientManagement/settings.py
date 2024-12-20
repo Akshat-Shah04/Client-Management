288fe00a079d8f23c5938331c5f1ce3dff044255
Akshat-Shah04 <shahakshat200@gmail.com>
2024-12-20 15:06:23 +0530

    Setting Up Project

--- env/ClientManagement/ClientManagement/settings.py
+++ env/ClientManagement/ClientManagement/settings.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+from decouple import Config, Csv
+
 
 from pathlib import Path
-
+from env import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+config = Config()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -73,13 +75,23 @@ WSGI_APPLICATION = 'ClientManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE'),
+        'USER': config('USERNAME'),
+        'PASSWORD': config('MYPASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT', cast=int),  # Cast the port as an integer
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
